0c2b66fb1b33637f3bb30599236f667819f88888
Ruksan karki <rukshankarki80@gmail.com>
2025-09-16 13:17:16 +0545

    first commit

---
+++ new.js
@@ -0,0 +1,22 @@
+// var hello = "Hello, world!";
+// console.log(hello);
+
+// let  hello = "Hello, world!";
+// hello = "Hello, everyone!";
+// hello = "Hello, universe!";
+
+// console.log(hello);
+
+
+
+
+// let name = "ruksan";
+// console.log(name);
+// name = "john";
+// console.log(name);
+
+const name = "ruksan";
+console.log(name);
+// const name = "john"; // error in const (it cannot be changed in const) 
+// name = "john";
+// console.log(name);
