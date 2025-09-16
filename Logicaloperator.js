5199b962a88ec1e9cc54623ff3089560884aa5fa
Ruksan karki <rukshankarki80@gmail.com>
2025-09-16 14:15:43 +0545

    Refactor logical operator examples and add admin access check

--- Logicaloperator.js
+++ Logicaloperator.js
@@ -3,3 +3,15 @@ let val2 = true;
 console.log(val && val2); // AND operator
 console.log(val || val2); // OR operator
 console.log(!val); // NOT operator
+
+
+
+
+
+
+let admin = false;
+if (admin) {
+    console.log("You have admin access");
+} else {
+    console.log("You do not have admin access");
+}
