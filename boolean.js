45f3e510300e5c98133efbb47353cd36ae3f0745
Ruksan karki <rukshankarki80@gmail.com>
2025-09-18 13:02:05 +0545

    Refactor object.js to uncomment and organize destructuring examples

---
+++ boolean.js
@@ -0,0 +1,11 @@
+let bool = true;
+let bool1 = false;
+console.log(typeof bool);
+console.log(typeof bool1);
+
+
+// undefined
+
+let val=null;
+val=12;
+console.log(val)
--- object.js
+++ object.js
@@ -1,57 +1,72 @@
 
-// Destructuring in Object
-// (.) use garne
+// // Destructuring in Object
+// // (.) use garne
 
 
-// array
-const arr = [ {
-    name: "ruksan",
-    Age: 24,
-    city: "Itahari"
+// // array
+// const arr = [ {
+//     name: "ruksan",
+//     Age: 24,
+//     city: "Itahari"
 
-} , {
-    name: "John",
-    Age: 30,
-    city: "New York"
-} ,  {
-    name: "Alice",
-    Age: 28,
-    city: "Los Angeles"
-}
-];
-
-
-
-// const { city } = arr[2];
-// console.log(city);
-
-// const { name, Age } = arr[1];
-// console.log(name, Age);
+// } , {
+//     name: "John",
+//     Age: 30,
+//     city: "New York"
+// } ,  {
+//     name: "Alice",
+//     Age: 28,
+//     city: "Los Angeles"
+// }
+// ];
 
 
-const arr1 =  {
-    name: "ruksan",
-    Age: 24,
-    address: {
-        city: "Itahari"
 
+// // const { city } = arr[2];
+// // console.log(city);
+
+// // const { name, Age } = arr[1];
+// // console.log(name, Age);
+
+
+// const arr1 =  {
+//     name: "ruksan",
+//     Age: 24,
+//     address: {
+//         city: "Itahari"
+
+//     }
+
+// };
+
+
+// // const {name , Age ,  address: { city } } = arr1;
+// // console.log( name , Age , city);
+
+// // // loop using to access object
+
+// const arr1 =  {
+//     name: "ruksan",
+//     Age: 24,
+//     address: {
+//         city: "Itahari"
+
+//     }
+// };
+
+
+
+
+let laptop = {
+    name: "Dell",
+    model: "Inspiron",
+    year: 2021,
+    specs: {
+        ram: "16GB",
+        storage: "512GB SSD"
     }
-
 };
-
-
-// const {name , Age ,  address: { city } } = arr1;
-// console.log( name , Age , city);
-
-// // loop using to access object
-
-const arr1 =  {
-    name: "ruksan",
-    Age: 24,
-    address: {
-        city: "Itahari"
-
-    }
-};
+console.log(laptop);
+console.log("The laptop name is :",laptop.name , "And the Details :" , laptop.specs);
 
 
