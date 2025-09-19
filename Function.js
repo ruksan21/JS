069b77593164c798d77ae55a050e86c4196b0564
Ruksan karki <rukshankarki80@gmail.com>
2025-09-19 12:36:40 +0545

    Add initial implementation of demo function and array example

---
+++ Function.js
@@ -0,0 +1,6 @@
+ // demo() parathesis aayeo bhane function call huncha
+function demo() { 
+    console.log("This is function Call"); 
+}
+demo();
+ 
---
+++ array.js
@@ -0,0 +1,19 @@
+// //array
+
+// let arr = [ 1, 2, 3, 4, 5 ]
+//         //  0  1  2  3  4  <= THe index pattern of array
+
+
+
+// console.log(arr);
+
+// arr[3] = 50;
+// console.log(arr);
+
+
+
+
+
+let arr1 = [ hari , ram, shyam, mohan]
+
+console.log(arr1)
