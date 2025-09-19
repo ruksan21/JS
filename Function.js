a7af7d938117d0ea449314ec6cca7beb33363d95
Ruksan karki <rukshankarki80@gmail.com>
2025-09-19 13:27:55 +0545

    Add functions for addition, subtraction, and arrow function example

--- Function.js
+++ Function.js
@@ -4,3 +4,54 @@ function demo() {
 }
 demo();
  
+
+// adding two number
+
+function add(){
+    console.log(2+3);
+}
+add();
+
+
+// Subbtraction 
+
+function sub(){
+    console.log("The subtract value is : " , 3-2);
+}
+sub();
+
+// Function with parameter
+
+// function sum(x, y,z);{
+//    let z
+//    z=x+y;
+//    return z;
+// }
+// sum(1,20);
+
+
+// const sub = function(j, k){
+//     console.log(j+K)
+// }
+
+
+
+// function demofun(){
+//     return "Hello World!"
+// }
+// let result = demofun()
+
+// function name(first_name, lastname , age){
+//     return "MY name is "+ first_name + "" + lastname ,"mine age is :" , age 
+// }
+// console.log (name ("Ruksan","Karki","age"));
+
+
+
+// arrow function
+const arrowFun = ( num1 , num2 )=>  
+console.log("the addition of num1 and num2 is :" , num1+num2)
+arrowFun( 6,9)
+
+
+
