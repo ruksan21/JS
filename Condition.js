0805a3ff47b39e73ff0a02abc3730201f526d842
Ruksan karki <rukshankarki80@gmail.com>
2025-09-21 12:46:10 +0545

    Add initial implementation of admin check and marksheet evaluation

---
+++ Condition.js
@@ -0,0 +1,30 @@
+const Isadmin = false;
+if (Isadmin){
+    console.log("Welcome to the admin login page");
+
+
+}
+else {
+    console.log("You are not admin ")
+}
+
+
+// marksheet 
+
+// const marksheet = 90;
+// if (marksheet>=90 && marksheet<=100){
+//     console.log("Congratulation you got A+")
+// }else if ( marksheet>= 80 && marksheet<=90){
+//     console.log("Congratulation you got A")
+// }else if ( marksheet>=70 && marksheet <=80) {
+//     console.log("Congratulation you got A")   
+// }
+
+
+
+// Ternary Operator
+
+let marksheet = 60
+marksheet>40?"you are pass ":"you are fail"
+
+
