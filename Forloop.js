e1ec2150aa01ef17a17749c085ca40793090edff
Ruksan karki <rukshankarki80@gmail.com>
2025-09-23 13:11:23 +0545

    Add initial HTML structure and create empty filter.js file

--- Forloop.js
+++ Forloop.js
@@ -1,3 +1,18 @@
-for (let i=0; i<10; i++){
-    console.log(i)
+// for (let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+let num = [1,2,3,4,5,6,7,8,9];
+for (let x of num ){
+    console.log(x*2)
+}
+
+
+
+// ObjectArr
+
+const aniarr={
+    name:"OX",
+    place: "",
+    rollNo: 21,
 }
