e70a7a73ab74e46f33db11a3b33b7d77dffd8b07
Ruksan karki <rukshankarki80@gmail.com>
2025-09-24 13:15:13 +0545

    Add initial HTML structure and JavaScript for dynamic h1 element creation

---
+++ DOM/index.js
@@ -0,0 +1,8 @@
+const { createElement } = 
+
+const demo = document.getElementById("demo")
+const container = document.getElementById("container")
+const newh1 = createElement ("h1")
+newh1.textContent = "This is newh1 element is created"
+console.log(newH1)
+demo.textContent="Hello World Textcontent"
